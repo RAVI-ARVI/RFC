6e7ffa363a89eb01308337e573faac16594d995c
Ravi <ravisankar@campx.in>
2024-10-05 17:50:32 +0530

    navigate

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -3,18 +3,17 @@ import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const AppLayout = () => {
-  const navigate =useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!Cookies.get("userlogin")) {
-      navigate('/login')
-      
+      navigate("/login");
+    } else {
+      navigate("/home");
     }
-    
-  },[])
+  }, []);
   return (
     <>
-      
       <Outlet />
     </>
   );
