33db1f4a6a093766ce61018e6f8f7d9cb2bf34f8
Ravi <ravisankar@campx.in>
2024-10-05 17:49:13 +0530

    log out added

--- src/Pages/Customers/AddLoanForm.jsx
+++ src/Pages/Customers/AddLoanForm.jsx
@@ -1,75 +1,76 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { api } from "@/utils/axios"
-import { LoaderIcon } from "lucide-react"
-import { useState } from "react"
-import { useMutation, useQueryClient } from "react-query"
-import { useParams } from "react-router-dom"
-import { toast } from "react-toastify"
-const defaultState={
-  loanType: '',
-  principalAmount: '',
-    repaymentAmount: '',
-    durationDays: '',
-    startDate: '',
-    endDate: '',
-    interestRate: '',
-  interestDuePeriod: '',
-    loanName:''
-    
-  
-}
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { api } from "@/utils/axios";
+import { LoaderIcon } from "lucide-react";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+const defaultState = {
+  loanType: "",
+  principalAmount: "",
+  repaymentAmount: "",
+  durationDays: "",
+  startDate: "",
+  endDate: "",
+  interestRate: "",
+  interestDuePeriod: "",
+  loanName: "",
+};
 
 const AddLoanForm = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const queryclient=useQueryClient()
-  const { id } = useParams()
-  const [customer, setCustomer] = useState(defaultState)
+  const queryclient = useQueryClient();
+  const { id } = useParams();
+  const [customer, setCustomer] = useState(defaultState);
 
+  const { mutate, isLoading } = useMutation(
+    (postbody) => {
+      return api.post(`/customer/${id}/loan`, postbody);
+    },
+    {
+      onSuccess: () => {
+        setIsDialogOpen(false);
+        queryclient.invalidateQueries("getcustomerdetails");
 
-  const { mutate,isLoading } = useMutation((postbody) => {
-    return api.post(`/customer/${id}/loan`, postbody)
-  }, {
-    onSuccess: () => {
-      setIsDialogOpen(false)
-      queryclient.invalidateQueries('getcustomerdetails')
-
-      toast.success("Loan Added  Successfully!")
-      setCustomer(defaultState)
+        toast.success("Loan Added  Successfully!");
+        setCustomer(defaultState);
+      },
     }
-  })
-
+  );
 
   const handleSubmit = () => {
-    console.log(customer,"this is customer")
     // const { name, phone, address } = customer;
 
- 
     // if (!name || !phone || !address) {
     //   toast.warn("Please fill in all fields.")
-  
+
     //   return;
     // }
- 
-   
-    mutate(customer)
- 
-  }
+
+    mutate(customer);
+  };
 
   const handleInputChange = (e) => {
-    
     const { name, value } = e.target;
-   
+
     setCustomer((prevState) => ({
       ...prevState,
       [name]: value,
@@ -77,66 +78,64 @@ const AddLoanForm = () => {
   };
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen} >
-    <DialogTrigger asChild>
-      <Button >Add Loan</Button>
-    </DialogTrigger>
-    <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Create Loan</DialogTitle>
-     
-      </DialogHeader>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button>Add Loan</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Loan</DialogTitle>
+        </DialogHeader>
         <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="loanName" className="text-right">
-          Loan Name
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="loanName" className="text-right">
+              Loan Name
             </Label>
-            
+
             <Input
               name="loanName"
               id="loanName"
-       
               onChange={handleInputChange}
-            defaultValue={customer.loanName}
-            className="col-span-3"
-          />
-          </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-          Loan Type
-                      </Label>
-            <Select name="loanType" onValueChange={(e) => {
-                setCustomer((prevState) => ({
-                  ...prevState,
-                 loanType: e,
-                }));
-              
-                      }} defaultValue={customer.loanType}  >
-      <SelectTrigger    className="col-span-3">
-                <SelectValue placeholder="Select Loan Type" />
-      </SelectTrigger>
-      <SelectContent    >
-        <SelectGroup >
-      
-          <SelectItem value="interest">Interest</SelectItem>
-          <SelectItem value="corporation">Corporation</SelectItem>
-        
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-          
+              defaultValue={customer.loanName}
+              className="col-span-3"
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="principalAmount" className="text-right">
-          Principal Amount
-          </Label>
+            <Label htmlFor="name" className="text-right">
+              Loan Type
+            </Label>
+            <Select
+              name="loanType"
+              onValueChange={(e) => {
+                setCustomer((prevState) => ({
+                  ...prevState,
+                  loanType: e,
+                }));
+              }}
+              defaultValue={customer.loanType}
+            >
+              <SelectTrigger className="col-span-3">
+                <SelectValue placeholder="Select Loan Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="interest">Interest</SelectItem>
+                  <SelectItem value="corporation">Corporation</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="principalAmount" className="text-right">
+              Principal Amount
+            </Label>
             <Input
               name="principalAmount"
-            id="principalAmount"
-            defaultValue={customer.principalAmount}
+              id="principalAmount"
+              defaultValue={customer.principalAmount}
               className="col-span-3"
               onChange={handleInputChange}
-          />
+            />
           </div>
           {/* <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="startDate" className="text-right">
@@ -151,130 +150,120 @@ const AddLoanForm = () => {
             className="col-span-3"
           />
           </div> */}
-           <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="startDate" className="text-right">
-          Start Date
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="startDate" className="text-right">
+              Start Date
             </Label>
-            
+
             <Input
               name="startDate"
               id="startDate"
-               placeholder="YYYY-MM-DD"
+              placeholder="YYYY-MM-DD"
               onChange={handleInputChange}
-            defaultValue={customer.endDate}
-            className="col-span-3"
-          />
+              defaultValue={customer.endDate}
+              className="col-span-3"
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="endDate" className="text-right">
-          End Date
-          </Label>
+            <Label htmlFor="endDate" className="text-right">
+              End Date
+            </Label>
             <Input
               name="endDate"
               id="endDate"
-             placeholder="YYYY-MM-DD"
+              placeholder="YYYY-MM-DD"
               onChange={handleInputChange}
-            defaultValue={customer.endDate}
-            className="col-span-3"
-          />
-            </div>
-          
-          {customer.loanType == "corporation" && <>
-            <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="durationDays" className="text-right">
-          Total  Days 
-          </Label>
-            <Input
-              name="durationDays"
-              id="durationDays"
-              onChange={handleInputChange}
-            defaultValue={customer.durationDays}
-            className="col-span-3"
-          />
+              defaultValue={customer.endDate}
+              className="col-span-3"
+            />
           </div>
-           
-          <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="repaymentAmount" className="text-right">
-          Repayment Amount 
-          </Label>
-            <Input
-              name="repaymentAmount"
-              id="repaymentAmount"
-              onChange={handleInputChange}
-            defaultValue={customer.address}
-            className="col-span-3"
-          />
-          </div>
-         </>}
-          
-          {
-            customer.loanType == "interest" && <>
-              
-          <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="interestRate" className="text-right">
-          InterestRate
-          </Label>
-            <Input
-              name="interestRate"
-              id="interestRate"
-              onChange={handleInputChange}
-            defaultValue={customer.interestRate}
-            className="col-span-3"
-          />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-          interestDuePeriod
-                      </Label>
-            <Select name="interestDuePeriod"
-              onValueChange={(e) => {
-                setCustomer((prevState) => ({
-                  ...prevState,
-                  interestDuePeriod: e,
-                }));
-                         
-            }}
-              defaultValue={customer.interestDuePeriod}   >
-      <SelectTrigger    className="col-span-3">
-        <SelectValue placeholder="Select  Due Period" />
-      </SelectTrigger>
-      <SelectContent    >
-        <SelectGroup >
-      
-          <SelectItem value="monthly">monthly</SelectItem>
-          <SelectItem value="yearly">yearly</SelectItem>
-        
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-          
-        </div>
-            
+
+          {customer.loanType == "corporation" && (
+            <>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="durationDays" className="text-right">
+                  Total Days
+                </Label>
+                <Input
+                  name="durationDays"
+                  id="durationDays"
+                  onChange={handleInputChange}
+                  defaultValue={customer.durationDays}
+                  className="col-span-3"
+                />
+              </div>
+
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="repaymentAmount" className="text-right">
+                  Repayment Amount
+                </Label>
+                <Input
+                  name="repaymentAmount"
+                  id="repaymentAmount"
+                  onChange={handleInputChange}
+                  defaultValue={customer.address}
+                  className="col-span-3"
+                />
+              </div>
             </>
+          )}
 
-          }
-      
-
-
-
-      </div>
+          {customer.loanType == "interest" && (
+            <>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="interestRate" className="text-right">
+                  InterestRate
+                </Label>
+                <Input
+                  name="interestRate"
+                  id="interestRate"
+                  onChange={handleInputChange}
+                  defaultValue={customer.interestRate}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  DuePay
+                </Label>
+                <Select
+                  name="interestDuePeriod"
+                  onValueChange={(e) => {
+                    setCustomer((prevState) => ({
+                      ...prevState,
+                      interestDuePeriod: e,
+                    }));
+                  }}
+                  defaultValue={customer.interestDuePeriod}
+                >
+                  <SelectTrigger className="col-span-3">
+                    <SelectValue placeholder="Select  Due Period" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="monthly">Monthly</SelectItem>
+                      <SelectItem value="yearly">Yearly</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </>
+          )}
+        </div>
         <DialogFooter>
-          {isLoading ? <Button
-                
-                className="w-full cursor-not-allowed opacity-50 "
-                
-            >
-             Loading... <LoaderIcon/>
-            </Button>:
-            <Button onClick={handleSubmit} className="w-full">Create Customer</Button>}
-         
-      </DialogFooter>
-    </DialogContent>
-  </Dialog>
-  )
-}
+          {isLoading ? (
+            <Button className="w-full cursor-not-allowed opacity-50 ">
+              Loading... <LoaderIcon />
+            </Button>
+          ) : (
+            <Button onClick={handleSubmit} className="w-full">
+              Create Loan
+            </Button>
+          )}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-
-
-
-export default AddLoanForm
+export default AddLoanForm;
--- src/Pages/Customers/AddTransactionForm.jsx
+++ src/Pages/Customers/AddTransactionForm.jsx
@@ -1,65 +1,71 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { api } from "@/utils/axios"
-import { LoaderIcon } from "lucide-react"
-import { useState } from "react"
-import { useMutation, useQueryClient } from "react-query"
-import { useParams } from "react-router-dom"
-import { toast } from "react-toastify"
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { api } from "@/utils/axios";
+import { LoaderIcon } from "lucide-react";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 // eslint-disable-next-line react/prop-types
 const AddTransactionForm = ({ loanData }) => {
-    const {id}=useParams()
-   
+  const { id } = useParams();
+
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const queryclient=useQueryClient()
-  
+  const queryclient = useQueryClient();
 
-  const { mutate,isLoading } = useMutation((postbody) => {
-    return api.post(`/customer/${id}/transaction`, postbody)
-  }, {
-    onSuccess: () => {
-      setIsDialogOpen(false)
-      queryclient.invalidateQueries('getcustomerdetails')
+  const { mutate, isLoading } = useMutation(
+    (postbody) => {
+      return api.post(`/customer/${id}/transaction`, postbody);
+    },
+    {
+      onSuccess: () => {
+        setIsDialogOpen(false);
+        queryclient.invalidateQueries("getcustomerdetails");
 
-      toast.success("Transaction  Created Successfully!")
+        toast.success("Transaction  Created Successfully!");
+      },
     }
-  })
+  );
   const [customer, setCustomer] = useState({
-    transactionType: '',
-    loanId: '',
-    amount: '',
-    paymentDate:''
-  })
+    transactionType: "",
+    loanId: "",
+    amount: "",
+    paymentDate: "",
+  });
 
   const handleSubmit = () => {
     const { transactionType, loanId, amount } = customer;
 
- 
     if (!transactionType || !loanId || !amount) {
-      toast.warn("Please fill in all fields.")
-  
+      toast.warn("Please fill in all fields.");
+
       return;
     }
- 
-   
-    mutate(customer)
- 
-  }
+
+    mutate(customer);
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-   
+
     setCustomer((prevState) => ({
       ...prevState,
       [name]: value,
@@ -67,48 +73,47 @@ const AddTransactionForm = ({ loanData }) => {
   };
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-    <DialogTrigger asChild>
-      <Button >    Add Transaction</Button>
-    </DialogTrigger>
-    <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Create Transaction</DialogTitle>
-        {/* <DialogDescription>
+      <DialogTrigger asChild>
+        <Button> Add Transaction</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Transaction</DialogTitle>
+          {/* <DialogDescription>
           Make changes to your profile here. Click save when you're done.
         </DialogDescription> */}
-      </DialogHeader>
-              <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="loanId" className="text-right">
-                      loanId
-                      </Label>
-                  
-                       <Select name="loanId"
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="loanId" className="text-right">
+              loanId
+            </Label>
+
+            <Select
+              name="loanId"
               onValueChange={(e) => {
                 setCustomer((prevState) => ({
                   ...prevState,
                   loanId: e,
                 }));
-                         
-            }}
-              defaultValue={customer.loanId}   >
-      <SelectTrigger    className="col-span-3">
-        <SelectValue placeholder="Select Loan" />
-      </SelectTrigger>
-      <SelectContent    >
-                              <SelectGroup >
-                                  {loanData?.map((item) => <SelectItem key={item?._id} value={item?._id}>{item?.loanName
- }</SelectItem>)}
-      
-          
-                                
-        
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-                  
-                      </div>
-        {/* <div className="grid grid-cols-4 items-center gap-4">
+              }}
+              defaultValue={customer.loanId}
+            >
+              <SelectTrigger className="col-span-3">
+                <SelectValue placeholder="Select Loan" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {loanData?.map((item) => (
+                    <SelectItem key={item?._id} value={item?._id}>
+                      {item?.loanName}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          {/* <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="transactionType" className="text-right">
            Type
           </Label>
@@ -121,73 +126,80 @@ const AddTransactionForm = ({ loanData }) => {
             className="col-span-3"
           />
         </div> */}
-                   <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="transactionType" className="text-right">
-                          Type
-                      </Label>
-                  
-                       <Select name="transactionType"
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="transactionType" className="text-right">
+              Type
+            </Label>
+
+            <Select
+              name="transactionType"
               onValueChange={(e) => {
                 setCustomer((prevState) => ({
                   ...prevState,
                   transactionType: e,
                 }));
-                         
-            }}
-              defaultValue={customer.transactionType}   >
-      <SelectTrigger    className="col-span-3">
-        <SelectValue placeholder="Select  Type" />
-      </SelectTrigger>
-      <SelectContent    >
-        <SelectGroup >
-      
-          <SelectItem value="corporation payment">Corporation Payment</SelectItem>
-                                  <SelectItem value='principal payment'>Principal Payment</SelectItem>
-                                  <SelectItem value='interest payment'>Interest Payment</SelectItem>
-        
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-                  
+              }}
+              defaultValue={customer.transactionType}
+            >
+              <SelectTrigger className="col-span-3">
+                <SelectValue placeholder="Select  Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="corporation payment">
+                    Corporation Payment
+                  </SelectItem>
+                  <SelectItem value="principal payment">
+                    Principal Payment
+                  </SelectItem>
+                  <SelectItem value="interest payment">
+                    Interest Payment
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
-         < div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="paymentDate" className="text-right">
-          Paid Date
-          </Label>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="paymentDate" className="text-right">
+              Paid Date
+            </Label>
             <Input
               name="paymentDate"
               id="paymentDate"
-               placeholder="YYYY-MM-DD"
+              placeholder="YYYY-MM-DD"
               onChange={handleInputChange}
-            defaultValue={customer.paymentDate}
-            className="col-span-3"
-          />
+              defaultValue={customer.paymentDate}
+              className="col-span-3"
+            />
           </div>
-                  
 
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="amount" className="text-right">
-          amount
-          </Label>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="amount" className="text-right">
+              amount
+            </Label>
             <Input
               name="amount"
-            id="amount"
-            defaultValue={customer.amount}
+              id="amount"
+              defaultValue={customer.amount}
               className="col-span-3"
               onChange={handleInputChange}
-          />
+            />
           </div>
-         
-      </div>
+        </div>
         <DialogFooter>
-          {isLoading? <Button className="w-full cursor-not-allowed opacity-50 ">
-       Loading... <LoaderIcon/>
-      </Button>:<Button onClick={handleSubmit} className="w-full">Create Customer</Button>}
-          
-      </DialogFooter>
-    </DialogContent>
-  </Dialog>
-  )
-}
+          {isLoading ? (
+            <Button className="w-full cursor-not-allowed opacity-50 ">
+              Loading... <LoaderIcon />
+            </Button>
+          ) : (
+            <Button onClick={handleSubmit} className="w-full">
+              Create Transaction
+            </Button>
+          )}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default AddTransactionForm
+export default AddTransactionForm;
